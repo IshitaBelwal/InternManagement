5765078f2fec35d4938b5b87011ebaba5b2cc1fa
Ishita Belwal <71529737+IshitaBelwal@users.noreply.github.com>
2023-09-08 02:25:07 +0530

    Update Admin.java

--- src/main/java/com/internProgramManagment/modal/Admin.java
+++ src/main/java/com/internProgramManagment/modal/Admin.java
@@ -4,5 +4,23 @@ public class Admin {
     private String username;
     private String password;
 
-    // Getters and setters
+   // Getter for username
+    public String getUsername() {
+        return username;
+    }
+
+    // Setter for username
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // Getter for password
+    public String getPassword() {
+        return password;
+    }
+
+    // Setter for password
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
